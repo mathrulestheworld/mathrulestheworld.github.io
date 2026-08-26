c29388bce3532ae43734934a62b9afe3b0a55981
mathrulestheworld <33874664+mathrulestheworld@users.noreply.github.com>
2026-08-26 03:14:42 +0000

    Deploying to gh-pages from @ mathrulestheworld/mathrulestheworld.github.io@c633b11b7b919e9cb1a086a605e9574a5f94d576 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,87 @@
+const searchPalette = document.querySelector("ninja-keys");
+
+if (searchPalette) {
+  const navigation = [{
+          id: "nav-about",
+          title: "about",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/";
+          },
+        },{
+          id: "nav-publications",
+          title: "publications",
+          description: "complete publication record, grouped by publication type and ordered reverse chronologically.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{
+          id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{
+          id: "nav-talks",
+          title: "talks",
+          description: "research talks and conference presentations in reverse chronological order.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/talks/";
+          },
+        },{
+          id: "nav-professional-service",
+          title: "professional service",
+          description: "peer-review service for theoretical computer science conferences and journals.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/service/";
+          },
+        },];
+
+  const profileLinks = [
+    {
+      id: "profile-google-scholar",
+      title: "Google Scholar",
+      section: "Profile",
+      handler: () => {
+        window.open("https://scholar.google.com/citations?user=dmo4-YMAAAAJ", "_blank");
+      },
+    },
+    {
+      id: "profile-github",
+      title: "GitHub",
+      section: "Profile",
+      handler: () => {
+        window.open("https://github.com/mathrulestheworld", "_blank");
+      },
+    },
+    {
+      id: "profile-cv",
+      title: "Curriculum Vitae PDF",
+      section: "Profile",
+      handler: () => {
+        window.open("/assets/pdf/cv.pdf", "_blank");
+      },
+    },
+  ];
+
+  const populateSearchPalette = async () => {
+    await customElements.whenDefined("ninja-keys");
+    searchPalette.data = navigation.concat(profileLinks);
+  };
+
+  window.openSearchModal = () => {
+    const $navbarNav = $("#navbarNav");
+    if ($navbarNav.hasClass("show")) {
+      $navbarNav.collapse("hide");
+    }
+    searchPalette.open();
+  };
+
+  populateSearchPalette();
+}
